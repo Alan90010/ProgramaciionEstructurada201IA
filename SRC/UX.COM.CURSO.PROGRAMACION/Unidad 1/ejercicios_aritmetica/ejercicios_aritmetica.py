7f4a493c0254c38aacd7e2df5fd7d0fbb6932911
Alan <dominzalan@gmail.com>
2026-03-17 07:29:58 -0600

    Aritmetica

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 1/ejercicios_aritmetica/ejercicios_aritmetica.py
@@ -0,0 +1,33 @@
+# Ejemplo de uso de operadores de aritmetica en Pythob
+
+def mostrar_operadores ():
+
+    # Suma
+    suma = 5 + 3
+    print("Suma 5 + 3 =", suma)
+
+    # Resta
+    resta = 10 - 4
+    print("Resta 10 - 4 =", resta)
+
+    # Multiplicacion
+    multiplicacion = 6 * 7
+    print("Multiplicacion 6 * 7 =", multiplicacion)
+
+    # Division
+    division_entera = 10 // 3
+    print("Division 10 // 3 =", division_entera)
+
+    # Modulo
+    modulo = 10 % 3
+    print("Modulo 10 % 3 =", modulo)
+
+    # Potencia
+    potencia = 2 ** 3
+    print("Potencia 2 ** 3 =", potencia)
+
+def main():
+    mostrar_operadores()
+
+if __name__ == "__main__":
+    main()
