ad57214468ac70092970dd19f89c30791c1c5782
Alan <dominzalan@gmail.com>
2026-03-23 08:28:57 -0600

    Examen FILTRADO

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 1/Examen/filtrado.py
@@ -0,0 +1,13 @@
+def filtrado():
+    w = float(input("Ingresa el Peso:"))
+    x = float(input("Ingresa Dato de Entrada: "))
+
+    Z = w * x
+
+    print("El valor es:", Z)
+
+def main():
+    filtrado()
+
+if __name__ == "__main__":
+    main()
--- SRC/UX.COM.CURSO.PROGRAMACION/Unidad 1/Examen/lotes.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 1/Examen/lotes.py
@@ -1,13 +1,20 @@
-total = 0
+def lotes():
+    total = 0
 
-while True:
+    while True:
 
-    lote = float(input("Cual es el tamaño del lote en MB? ")) 
-    total = total + lote
+        lote = float(input("Cual es el tamaño del lote en MB? ")) 
+        total = total + lote
 
-    print("El tamaño es:", total ,"MB")
+        print("El tamaño es:", total ,"MB")
 
-    if total > 2500:
-        print("Out of Memory")
-        print("El total no puede superar los 2500 MB")
-        break
+        if total > 2500:
+            print("Out of Memory")
+            print("El total no puede superar los 2500 MB")
+            break
+
+def main():
+    lotes()
+
+if __name__ == "__main__":
+    main()
