0670c7f2ca083d1868440bc7bc942fc0347f42c4
Alan <dominzalan@gmail.com>
2026-03-23 08:02:57 -0600

    Examen LOTES

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 1/Examen/lotes.py
@@ -0,0 +1,13 @@
+total = 0
+
+while True:
+
+    lote = float(input("Cual es el tamaño del lote en MB? ")) 
+    total = total + lote
+
+    print("El tamaño es:", total ,"MB")
+
+    if total > 2500:
+        print("Out of Memory")
+        print("El total no puede superar los 2500 MB")
+        break
