d5f79b3ca7cf2bb4f91f1aabda39f49bd7376836
Alan <dominzalan@gmail.com>
2026-04-07 07:46:01 -0600

    Ejercicio 5

--- SRC/UX.COM.CURSO.PROGRAMACION/Unidad 2/mermaid/ejercicio_05.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 2/mermaid/ejercicio_05.py
@@ -1,21 +1,19 @@
 """
-    Acumulacion Generica
+    Algoritmo de Acumulacion Generica
 """
-
 def Acumulacion():
-    suma = 0
-    while suma < 500:
-        numero = int(input("Ingrese un numero: "))
-        if numero >= 10 and numero <= 50: 
-            suma += numero
+    suma=0
+    while True:
+        numero = int(input("Ingresa numero:"))
+        if numero >= 10 and numero <= 50:
+            suma += numero 
         else:
             break
-
     return suma
 
 def main():
     resultado = Acumulacion()
-    print("La suma acumulada es: ", resultado)
+    print("La acumulacion es: ", resultado)
 
 if __name__ == "__main__":
     main()
