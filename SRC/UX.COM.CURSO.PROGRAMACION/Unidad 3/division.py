1cf70c3bca2bb97a80fe0ffc813d2ff110fe6a90
Alan <dominzalan@gmail.com>
2026-04-21 08:12:59 -0600

    Division

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 3/division.py
@@ -0,0 +1,24 @@
+def division_entera(dividendo, divisor):
+    if divisor == 0:
+        return "ERROR, Esta division no es posible"
+    
+    cociente = 0
+    resto = dividendo
+
+    while resto >= divisor:
+        resto = resto - divisor    
+        cociente = cociente + 1    
+
+    return cociente, resto
+
+dividendo = int(input("Ingresa el dividendo: "))
+divisor   = int(input("Ingresa el divisor: "))
+
+cociente, resto = division_entera(dividendo, divisor)
+print(f"{dividendo} ÷ {divisor} = {cociente}, resto: {resto}")
+
+def main():
+    division_entera(dividendo, divisor)
+
+if __name__ == "__main__":
+    main()
