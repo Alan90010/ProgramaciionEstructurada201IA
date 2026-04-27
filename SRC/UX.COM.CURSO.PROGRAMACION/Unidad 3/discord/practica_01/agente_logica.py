2ba97e82db407acd189df2c22dd25d483e6046b6
Alan <dominzalan@gmail.com>
2026-04-27 07:42:03 -0600

    Conceptos basicos

--- SRC/UX.COM.CURSO.PROGRAMACION/Unidad 3/discord/practica_01/agente_logica.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 3/discord/practica_01/agente_logica.py
@@ -6,20 +6,20 @@ def procesar_pregunta(mensasje_usuario):
 
     #2.Base de conocimiento (Diccionario)
     conocimiento = {
-        #Concepto de Estructura de Control
-        "if": "La sentencia 'if' es una condicional. Permite que el programa tome decisiones basandose en una condicion booleana.",
+# Concepto de Estructura de Control
+"if": "La sentencia 'if' es una estructura condicional que permite ejecutar un bloque de codigo solo si se cumple una condicion (True).",
 
-        #Tipos de Datos
-        "int": "Representa un numero entero (ej.5, -10, 0). No tienen parte decimal.",
+# Tipos de Datos
+"int": "Representa numeros enteros (ej. 5, -10, 0). No incluyen parte decimal.",
+ 
+# Funciones y modularidad
+"def": "Es la palabra reservada en Python que se utiliza para definir funciones, permitiendo reutilizar codigo.",
 
-        #Funciones y modularidad
-        "def": "Es la palabra reservada para definir una funcion en python",
+# Operadores y Sintaxis
+"print": "Funcion que muestra informacion en la consola o salida estandar.",
 
-        #Operadores y Sintaxis
-        "print": "Funcion que muestra informacion en la consola o salida estandar",
-
-        #Conceptos de Programacion Estructurada
-        "algoritmo": "Es una serie de pasos ordenados y finitos para resolver un problema"
+# Conceptos de Programacion Estructurada
+"algoritmo": "Es un conjunto de pasos ordenados, logicos y finitos que permiten resolver un problema."
     }
 
     #3.Logica de busqueda
