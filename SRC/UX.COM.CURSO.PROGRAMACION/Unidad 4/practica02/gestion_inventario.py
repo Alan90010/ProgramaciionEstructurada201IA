b424435ec94fef74b6a8dcd79b81b820ee145deb
Alan90010 <dominzalan@gmail.com>
2026-05-19 07:35:25 -0600

    Funciones

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 4/practica02/gestion_inventario.py
@@ -0,0 +1,8 @@
+def imprimir():
+    print("Funciones Externas(Bibliotecas)")
+
+def main():
+    imprimir()
+
+if __name__ == "__main__":
+    main()
--- SRC/UX.COM.CURSO.PROGRAMACION/Unidad 4/practica03/gestion_tiempo.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/Unidad 4/practica03/gestion_tiempo.py
@@ -1,1 +1,8 @@
-def 
+def imprimir():
+    print("Funciones Externas(Bibliotecas)")
+
+def main():
+    imprimir()
+
+if __name__ == "__main__":
+    main()
